2d0e96e3474e8cbdb673f63675ba72da99972618
Fernando <fmoya97.fm@gmail.com>
2020-02-24 20:14:15 -0300

    editarLeerDatosTablas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'App-Tareas';
   empleados= [
-    {'name': 'Fernando', cargo: 'manager'},
-    {'name': 'Juan', cargo: 'designer'},
-    {'name': 'Carlos', cargo: 'programmer'}
+    {'name': 'Fernando', cargo: 'manager', email:'fmoya97@gmail.com'},
+    {'name': 'Juan', cargo: 'designer', email:'hola@gmail.com'},
+    {'name': 'Carlos', cargo: 'programmer', email:'Code@gmail.com'}
   ];
 
   model: any = {}; //datos temporales
+  model2: any = {};
   addEmpleado(): void{
     this.empleados.push(this.model);
   }
@@ -21,13 +22,16 @@ export class AppComponent {
   deleteEmpleado(): void{
 
   }
-
-  editEmpleado(): void{
-
+  indice;
+  editEmpleado(i): void{
+    this.model2.name= this.empleados[i].name;
+    this.model2.cargo= this.empleados[i].cargo;
+    this.model2.email=this.empleados[i].email;
+    this.indice=i;
   }
 
   updateEmpleado(): void{
-
+    console.log(this.model2);
   }
 
 }
